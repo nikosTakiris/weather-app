1c287869dc322dda13924f3bfbb8f6f621e0da2a
Nikos Takiris <nik_takiris@yahoo.gr>
2019-03-02 07:37:09 +0200

    https

--- functions.js
+++ functions.js
@@ -89,8 +89,7 @@ let weather_form = document.querySelector(".weather_form");
 let search_button = document.querySelector(".search_button");
 search_button.addEventListener('click', getWeather);
 
-function getWeather(e) {
-  e.preventDefault();
+function getWeather() {
   let warning = document.querySelector(".warning");
   let apiKey = "466bb530a7d426fb42b230fdda00d5dc";
   let city = document.querySelector(".city");
